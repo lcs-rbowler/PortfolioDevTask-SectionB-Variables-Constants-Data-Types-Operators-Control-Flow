1e3c39c5839f386fd55c6910ed14ef645a09eae2
Bowler, Ryan <rbowler@lcs-mba2899.lcs.on.ca>
2017-12-19 09:50:08 -0500

    Question 5

--- Portfolio Development Task.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
@@ -16,7 +16,19 @@ let hasGreenAndWhiteBuildings = true
 
 // Answer question 5 below
 
-
+if hasWaterfrontOnLake == true {
+    if hasBoysAndGirlsAttending == true {
+        if hasGreenAndWhiteBuildings == true {
+            print("Must be Lakefield!")
+        } else {
+            print("Hmm, need more data to be sure.")
+        }
+    } else {
+        print("Hmm, need more data to be sure.")
+    }
+} else {
+    print("Hmm, need more data to be sure.")
+}
 /*:
  ## Now share your understanding
  
